205402949d46f78ec0ec7208f31d3e684ca320f8
Mahendra Kalkura <mahendra@kalkura.com>
2016-01-30 15:59:55 +0530

    lib/websockets/utilities.ex

--- lib/websockets/utilities.ex
+++ lib/websockets/utilities.ex
@@ -14,15 +14,22 @@ defmodule WebSockets.Utilities do
     Kernel.spawn(fn -> Logger.info("[#{get_module(module)}] [#{get_direction(direction)}] #{subject}") end)
   end
 
-  def get_direction(direction) do
-    :io_lib.format("~-3s", [direction])
+  def get_direction(string) do
+    :io_lib.format("~-3s", [string])
   end
 
-  def get_id(id) do
-    :io_lib.format("~9B", [id])
+  def get_id(integer) when is_integer(integer) do
+    :io_lib.format("~9B", [integer])
   end
 
-  def get_module(module) do
-    :io_lib.format("~-8s", [module])
+  def get_id(pid) when is_pid(pid) do
+    case Clients.select_one(pid) do
+      {:ok, id} -> id
+      _ -> 0
+    end
+  end
+
+  def get_module(string) do
+    :io_lib.format("~-8s", [string])
   end
 end
