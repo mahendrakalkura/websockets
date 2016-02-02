6715b2ae5cfbf907f9545b239aaa0489dcc65ea6
Mahendra Kalkura <mahendra@kalkura.com>
2016-02-03 02:21:02 +0530

    lib/websockets/supervisor.ex

--- lib/websockets/supervisor.ex
+++ lib/websockets/supervisor.ex
@@ -19,11 +19,37 @@ defmodule WebSockets.Supervisor do
     {:ok, _} = :cowboy.start_http(
       :http,
       100,
-      [{:port, Application.get_env(:websockets, :port)}],
-      [{:env, [{:dispatch, :cowboy_router.compile([{:_, [{"/", Router, []}]}])}]}]
+      [
+        {:port, Application.get_env(:websockets, :port)}
+      ],
+      [
+        {
+          :env,
+          [
+            {
+              :dispatch,
+              :cowboy_router.compile(
+                [
+                  {
+                    :_,
+                    [
+                      {"/", Router, []}
+                    ]
+                  }
+                ]
+              )
+            }
+          ]
+        }
+      ]
     )
     Spec.supervise(
-      [Spec.worker(Clients, []), Spec.worker(RabbitMQ, []), Spec.worker(Repo, [])], strategy: :one_for_one
+      [
+        Spec.worker(Clients, []),
+        Spec.worker(RabbitMQ, []),
+        Spec.worker(Repo, [])
+      ],
+      strategy: :one_for_one
     )
   end
 end
