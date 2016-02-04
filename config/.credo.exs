862f264b091bcaf9159aab871f05ee8c11aef9d9
Mahendra Kalkura <mahendra@kalkura.com>
2016-02-05 00:42:13 +0530

    config/.credo.exs

--- config/.credo.exs
+++ config/.credo.exs
@@ -18,7 +18,7 @@
         {Credo.Check.Readability.TrailingBlankLine},
         {Credo.Check.Readability.TrailingWhiteSpace},
         {Credo.Check.Readability.VariableNames},
-        {Credo.Check.Refactor.ABCSize},
+        {Credo.Check.Refactor.ABCSize, max_size: 50},
         {Credo.Check.Refactor.CaseTrivialMatches},
         {Credo.Check.Refactor.CondStatements},
         {Credo.Check.Refactor.CyclomaticComplexity},
